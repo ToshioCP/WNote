299569497fc138b4a5883e4fbd278c48cf959f59
ToshioCP <lxboyjp@gmail.com>
2017-06-20 21:51:20 +0900

    Bug(image helper) fixed. Upgrade epub controller.

--- app/controllers/epubs_controller.rb
+++ app/controllers/epubs_controller.rb
@@ -9,8 +9,6 @@ class EpubsController < ApplicationController
   end
 
   def show
-#   イメージファイルの配列
-    @image_ids = []
 #   epubデータ（本文、目次）の生成、イメージファイルの配列の生成
     mkepub
 #   表紙とCSSデータの生成
@@ -38,10 +36,12 @@ class EpubsController < ApplicationController
       ['META-INF/container.xml', @container_data, 'w'],
       ['OEBPS/book.opf', @opf_data, 'w'],
       ['OEBPS/toc.xhtml', @toc_data, 'w'],
-      ['OEBPS/book.xhtml', @xhtml_data, 'w'],
       ['OEBPS/images/cover.jpg', @cover_image, 'wb'],
       ['OEBPS/css/text.css', @css, 'w']
     ]
+    @xhtml_files.each do |xhtml_data|
+      files << ['OEBPS/' + xhtml_data[0], xhtml_data[1], 'w']
+    end
 #   zip圧縮
 #   注意　：　send_fileは非同期に実行されるようなので、zipファイルをすぐに削除することはできない
 #            以上から、一時ファイルはそのまま残すことにした。逆に始める時に以前の残りかすがある可能性に注意。
@@ -82,7 +82,90 @@ class EpubsController < ApplicationController
       end
     end
 
+=begin rdoc
+= CSSのクラスについて
+== 本文中のクラス
+
+本文中のsection、noteの見出しはh2, h3タグを用いている。
+このクラスを用いて、セクション、ノート見出しのデザインを変更することが出来る。
+
+- h2.section セクション見出し
+- h3.note ノート見出し
+
+各セクション、ノートの見出しは、任意のタグのid属性でリンク先を表すようになっている。
+（HTML4までは、aタグのname属性を用いていた）。
+
+以上を実際のhtmlにすると、
+
+  <h2 class="section" id="section5">セクション見出しについて</h2>
+  <h3 class="note" id="note23">ノート見出しについて</h3>
+
+html目次のsection、noteのリンクはdivタグにクラスを指定している。
+
+- div.section 目次の中のセクションへのリンクを囲む
+- div.note 目次の中のノートへのリンクを囲む
+
+以上を実際のhtmlにすると、
+
+  <div class="section"><a href="section5.html#section5">セクション見出しについて</a></div>
+  <div class="note"><a href="section5.html#note23">ノート見出しについて</a></div>
+
+=end
     def mkepub
+      # initialize RedCarpet
+      renderer = Redcarpet::Render::XHTML.new(render_options = {escape_html: true})
+      markdown = Redcarpet::Markdown.new(renderer, no_intra_emphasis: true, tables: true, lax_spacing: true, space_after_headers: true)
+      # (1)opfファイルのid, href (2)xhtmlファイル (3)目次のリンク の生成
+      ids_hrefs = [["booktoc", "booktoc.xhtml"]]
+      case @article.language
+        when "en" then
+          t = "Table of Contents"
+        when "ja" then
+          t = "目次"
+        else
+          t = "Table of Contents" # English is default
+      end
+      html_toc = "<div id=\"title\">#{@article.title}</div>\n" # html目次
+      html_toc = "<div id=\"author\">#{@article.author}</div>\n" # html目次
+      html_toc = "<h1 id=\"html_toc\">#{t}</h1>\n" # html目次
+      toc = "<ol>\n<li><a href=\"booktoc.xhtml#html_toc\">#{t}</a></li>\n" # 論理目次
+      @xhtml_files = []
+      @image_ids = [] #   イメージファイルの配列
+      @article.ordered_sections.each do |section|
+        #opf
+        ids_hrefs << ["book#{section.id}", "book#{section.id}.xhtml"]
+        # table of contents
+        html_toc << "<div class=\"section\"><a href=\"book#{section.id}.xhtml#section#{section.id}\">#{section.heading}</a></div>\n"
+        toc << "<li><a href=\"book#{section.id}.xhtml#section#{section.id}\">#{section.heading}</a></li>\n"
+        toc << "<ul>\n"
+        # body
+        xhtml_data = mkheader section.heading
+        xhtml_data << "<h2 class=\"section\" id=\"section#{section.id}\">#{section.heading}</h2>\n"
+        section.ordered_notes.each do |note|
+          # table of contents
+          html_toc << "<div class=\"note\"><a href=\"book#{section.id}.xhtml#note#{note.id}\">#{note.title}</a></div>\n"
+          toc << "<li><a href=\"book#{section.id}.xhtml#note#{note.id}\">#{note.title}</a></li>\n"
+          # body
+          xhtml_data << "<h3 class=\"note\" id=\"note#{note.id}\">#{note.title}</h3>\n"
+          text, image_ids = evaluate_for_epub(parse(markdown.render(note.text)))
+          xhtml_data << text
+          # image
+          @image_ids.concat image_ids
+        end
+        # table of contents
+        toc << "</ul>\n"
+        # body
+        xhtml_data << "</body>\n</html>\n"
+        @xhtml_files << ["book#{section.id}.xhtml", xhtml_data]
+      end
+      # table of contents
+      toc << "</ol>\n"
+      # html目次を@xhtml_filesの先頭へ
+      xhtml_data = mkheader "Table of Contents"
+      xhtml_data << html_toc
+      xhtml_data << "</body>\n</html>\n"
+      @xhtml_files.insert 0, ["booktoc.xhtml", xhtml_data]
+
       # container.xml data
       @container_data = <<EOS
 <?xml version=\"1.0\" encoding=\"UTF-8\"?>
@@ -92,6 +175,7 @@ class EpubsController < ApplicationController
   </rootfiles>
 </container>
 EOS
+
       # opf file data
       @opf_data = <<EOS
 <?xml version=\"1.0\" encoding=\"UTF-8\"?>
@@ -107,66 +191,51 @@ EOS
   <manifest>
     <item id=\"cimage\" media-type=\"image/jpeg\" href=\"images/cover.jpg\" properties=\"cover-image\"/>
     <item id=\"toc\" properties=\"nav\" href=\"toc.xhtml\" media-type=\"application/xhtml+xml\"/>
-    <item id=\"book\" href=\"book.xhtml\" media-type=\"application/xhtml+xml\"/>
+EOS
+      ids_hrefs.each do |id_href|
+        @opf_data << "    <item id=\"#{id_href[0]}\" href=\"#{id_href[1]}\" media-type=\"application/xhtml+xml\"/>\n"
+      end
+      @opf_data << <<EOS
   </manifest>
   <spine>
-    <itemref idref=\"book\"/>
+EOS
+      ids_hrefs.each do |id_href|
+        @opf_data << "    <itemref idref=\"#{id_href[0]}\"/>\n"
+      end
+      @opf_data << <<EOS
   </spine>
 </package>
 EOS
+
       # toc file data
       @toc_data = <<EOS
 <?xml version=\"1.0\" encoding=\"UTF-8\"?>
 <!DOCTYPE html>
 <html xmlns=\"http://www.w3.org/1999/xhtml\" xmlns:epub=\"http://www.idpf.org/2007/ops\" lang=\"#{@article.language}\" xml:lang=\"#{@article.language}\">
 <head>
+<title>Table of Contents</title>
 </head>
 <body>
 <nav epub:type=\"toc\">
-<ol>
+#{toc}
+</nav>
+</body>
+</html>
 EOS
-      @article.ordered_sections.each do |section|
-        @toc_data << "<li><a href=\"book.xhtml#section#{section.id}\">#{section.heading}</a></li>\n"
-        @toc_data << "<ol>\n"
-        section.ordered_notes.each do |note|
-          @toc_data << "<li><a href=\"book.xhtml#note#{note.id}\">#{note.title}</a></li>\n"
-        end
-        @toc_data << "</ol>\n"
-      end
-      @toc_data << "</ol>\n</nav>\n</body>\n</html>\n"
-      # xhtml content data
-      renderer = Redcarpet::Render::XHTML.new(render_options = {escape_html: true})
-      markdown = Redcarpet::Markdown.new(renderer, no_intra_emphasis: true, tables: true, lax_spacing: true, space_after_headers: true)
-      @xhtml_data = <<EOS
+
+    end
+
+    def mkheader title
+      return <<EOS
 <?xml version=\"1.0\" encoding=\"utf-8\"?>
 <!DOCTYPE html>
 <html xmlns=\"http://www.w3.org/1999/xhtml\" xmlns:xml=\"http://www.w3.org/XML/1998/namespace\" xml:lang=\"#{@article.language}\" lang=\"#{@article.language}\">
 <head>
 <link rel=\"stylesheet\" type=\"text/css\" href=\"./css/text.css\"/>
+<title>#{title}</title>
 </head>
 <body>
-<div id=\"title\">#{@article.title}</div>
-<div id=\"author\">#{@article.author}</div>
-<div id=\"toc\">目次</div>
 EOS
-      @article.ordered_sections.each do |section|
-        @xhtml_data << "<div class=\"section\"><a href=\"#section#{section.id}\">#{section.heading}</a></div>\n"
-        @xhtml_data << "<ul>\n"
-        section.ordered_notes.each do |note|
-          @xhtml_data << "<li><a href=\"#note#{note.id}\">#{note.title}</a></li>\n"
-        end
-        @xhtml_data << "</ul>\n"
-      end
-      @article.ordered_sections.each do |section|
-# html5 ではname属性が廃止され、id属性を使うことになった
-        @xhtml_data << "<a id=\"section#{section.id}\"><h2>#{section.heading}</h2></a>\n"
-        section.ordered_notes.each do |note|
-          @xhtml_data << "<a id=\"note#{note.id}\"><h3>#{note.title}</h3></a>\n"
-          text, image_ids = evaluate_for_epub(parse(markdown.render(note.text)))
-          @xhtml_data << text
-          @image_ids.concat image_ids
-        end
-      end
-      @xhtml_data << "</body>\n</html>\n"
     end
+
 end
--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,86 +1,103 @@
 module ImagesHelper
 
-# 画像を表すのに画像名を@で囲む構文で対応することにする。
-# @を選んだのは、Markdownで使われていない文字だから。
-# @自身を出力したいときもあるので、以下のようなルールを設けた
-#
-# 1. 行単位で構文解析をする
-# 2. @で画像名を囲むパターンがなければ、その行はそのまま出力される
-# 3. @で画像を囲むパターンがある行については、次のように対応する
-# 3-1. @@のように、アットマークが2つ続いたら、それは1つの@(アットマーク自身）を表すものとする
-# 3-2. 1個の@は画像名を囲んでいる場合は、画像を表す部分であると認識する
-# 3-3. 先頭から上記3-2で解析して、@が1つ残った場合は、それは画像ではないので、@自身を表す
-# 以下に例をあげる
-# 解析の結果文字の部分は:L、画像を表す場合は:Cとなっている
+=begin rdoc
+画像を表すのに画像名を@で囲む構文で対応することにする。
+@を選んだのは、Markdownで使われていない文字だから。
+@自身を出力したいときは@@と@を2個連続することにする。
+しかし、@@@のように3個連続の場合、@自身を表す@2つのセットが前の2つなのか後の2つなのか、はっきりしなければならないので、以下のようなルールを設けた。
 
-# This is @image@.  =>  [[:L, "This is "], [:C, "image"], [:L, "."]]
-# This is not @@image@@.  =>  [[:L, "This is not @image@."]]
-# @@image@@ translates @image@ but 1 @ is not changed.  =>  [[:L, "@image@ translates "], [:C, "image"], [:L, " but 1 @ is not changed."]]
-# @ is at sign.  =>  [[:L, "@ is at sign."]]
-# @@ is two at signs.  =>  [[:L, "@@ is two at signs."]]
+1. 行単位で構文解析をする
+2. 先頭の文字から順に1文字ずつ調べていく
+3. @が2個連続したらそれは文字の@（1個）とする
+4. @の次に@以外の文字が続きその後また@が続いたら、それは画像の名前であると解釈する（@は除かれる）
+5. @以外の文字はその文字自身を表す
+6. 3,4,5については先頭から調べてそれぞれに一致するものが見つかるごとに結果を出力する
+7. 4の解析の途中で行が終了した場合は、4の始まりの@は@自身を表す
 
-# 上記例をテストするための文字列（text_to_nodesの中身を取り出して、単体でテストする）
-# text = "This is @image@.\nThis is not @@image@@.\n@@image@@ translates @image@ but 1 @ is not changed.\n@ is at sign.\n@@ is two at signs.\n"
+この解析のためにメソッドparseの中では状態遷移を表す変数modeを使っている。
+
+[mode=0] @が出る前の状態、または「@ファイル名@」が解析し終わって、はじめに戻った状態
+[mode=1] @が現れた直後
+[mode=2] @の次に@以外の文字列が出てから次の@（または行末）が出る間までの状態
+[mode=3] 終了状態
+
+mode=1と2を区別することによって、プログラムが分かりやすくなる。
+
+以下に例をあげる
+解析の結果文字の部分は:L、画像を表す場合は:Cとして、配列で出力を表している。
+
+  This is @image@.  =>  [[:L, "This is "], [:C, "image"], [:L, "."]]
+  This is not @@image@@.  =>  [[:L, "This is not @image@."]]
+  @@image@@ translates @image@ but 1 @ is not changed.  =>  [[:L, "@image@ translates "], [:C, "image"], [:L, " but 1 @ is not changed."]]
+  @ is at sign.  =>  [[:L, "@ is at sign."]]
+  @@@@ is two at signs.  =>  [[:L, "@@ is two at signs."]]
+  @@@sample@@@ => [[:L, "@"], [:C, "sample], [:L, "@"]]
+  これは[[:C, "@sample@"]]や、[[:L, "@"], [:C, "sample@"]]ではない。前から1文字ずつ順に解析すると上記のようになる
+
+上記例をテストするための文字列（text_to_nodesの中身を取り出して、単体でテストする）
+
+  text = "This is @image@.\nThis is not @@image@@.\n@@image@@ translates @image@ but 1 @ is not changed.\n@ is at sign.\n@@@@ is two at signs.\n@@@sample@@@\n"
+=end
 
   def parse(text)
     nodes = []
     text.each_line do |line|
-      if ! (line =~ /@.+@/)
-        node = []
-        node[0] = [:L, line.chomp]
-      else
-    # 字句解析
-        temp = line.chomp.split /@@/
-        temp.map! {|t| t.split /@/}
-        tokens = []
-        temp.each do |t1|
-          t1.each do |t2|
-            tokens << [:L, t2]
-            tokens << [:C, '@']
-          end
-          tokens.delete_at(tokens.length-1)
-          tokens << [:L, '@']
-        end
-        tokens.delete_at(tokens.length-1)
-    # 構文解析
-        node = []
-        s = ""
-        mode = :start
-        tokens.each do |t|
-          case t[0]
-            when :L then
-              s = s + t[1]
-              if mode != :C
-                mode = :L
-              end
-            when :C then
-              if mode == :C
-                node << [:C, s]
-#               if s == "" then "Syntax error" end
-                s = ""
-                mode = :start
-              else
-                if s != ""
-                  node << [:L, s]
+      node = [] # output
+      s = "" # buffer
+      fn = "" # buffer for image file name
+      mode = 0 # 0 @が出る前の状態 1 @が現れた直後 2 @の次に@以外の文字列が出て、その後次の@が出ていない状態 3 終了状態
+      line.each_char do |c|
+        case mode
+          when 0 then
+            case c
+              when "@" then
+                mode = 1
+              when "\n" then
+                if ! s.empty?
+                  node << [:L, s.dup]
+                  s.clear
                 end
-                s = ""
-                mode = :C
-              end
-            else
-#             Unexpexcted error
-           end
-        end
-        if mode == :C
-          s = '@' + s
-          if node[node.length-1][0] == :L
-            node[node.length-1][1] << s
+                mode = 3
+              else
+                s << c
+                mode = 0 #変わっていないので、なくても良い
+            end
+          when 1 then
+            case c
+              when "@" then
+                s << "@"
+                mode = 0
+              when "\n" then
+                node << [:L, s+"@"]
+                s.clear
+                mode = 3
+              else
+                fn = c
+                mode = 2
+            end
+          when 2 then
+            case c
+              when "@" then
+                if ! s.empty?
+                  node << [:L, s.dup]
+                  s.clear
+                end
+                node << [:C, fn.dup]
+                fn.clear
+                mode = 0
+              when "\n" then
+                node << [:L, s+"@"+fn]
+                s.clear
+                fn.clear
+                mode = 3
+              else
+                fn << c
+                mode = 2 #変わっていないので、なくても良い
+            end
+          when 3 then
+            #Unexpected error
           else
-            node << [:L, s]
-          end
-        elsif mode == :L
-          node << [:L, s]
-        else
-#          do nothing. maybe mode == :start
+            #Unexpected error
         end
       end
       nodes << node
@@ -128,7 +145,7 @@ module ImagesHelper
             w = width ? "width = \"#{width}\"" : ""
             h = height ? "height = \"#{height}\"" : ""
             c = cl ? "class = \"#{cl}\"" : ""
-            line = line + "<img id=\"image_#{name}\" src=\"./images/b_#{name}.jpg\" " + w + " " + h + " />"
+            line = line + "<img id=\"image_#{name}\" src=\"./images/b_#{name}.jpg\" " + w + " " + h + " " + c + " />"
             image_ids << image.id
           end
         else
